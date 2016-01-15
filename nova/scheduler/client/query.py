41d4320cebd32c0b9b0c26ad3d8efa45c9742f67
Yingxin <yingxin.cheng@intel.com>
2016-01-15 14:46:38 +0000

    fix issues

--- nova/scheduler/client/query.py
+++ nova/scheduler/client/query.py
@@ -105,5 +105,6 @@ class SchedulerQueryClient(object):
     def notify_scheduler(self, context, compute, scheduler):
         self.scheduler_rpcapi.notify_scheduler(context, compute, scheduler)
 
-    def send_commit(self, context, compute, scheduler):
-        self.scheduler_rpcapi.send_commit(context, compute, scheduler)
+    def send_commit(self, context, commit, compute, scheduler):
+        self.scheduler_rpcapi.send_commit(
+            context, commit, compute, scheduler)
